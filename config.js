7b5abb00eebe6761ab8a93dd9f5150fdf87bf584
vodanh2806 <vodanh2806@gmail.com>
2025-04-11 23:21:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "GlaceYT",
-      password: "glaceyt",
-      host: "193.226.78.187",
-      port:  9372,
+      name: "Nakhun,
+      password: "lavalink",
+      host: "194.102.181.219",
+      port:  3827,
       secure: false
     }
   ]
